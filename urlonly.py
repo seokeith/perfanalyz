a2c82ac932ecdd3f64da0a0c22da8994de4c753f
seokeith <141214829+seokeith@users.noreply.github.com>
2023-08-08 08:55:32 +0100

    Update urlonly.py

--- urlonly.py
+++ urlonly.py
@@ -20,52 +20,58 @@ def validate_data(data):
     return "Data is valid!"
 
 def main():
-    st.title("URL Data Analysis")
-    uploaded_file = st.sidebar.file_uploader("Upload a CSV file", type="csv")
+    st.title("CSV Data Upload and Display")
+
+    # Upload CSV
+    uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
 
     if uploaded_file:
         try:
             data = pd.read_csv(uploaded_file)
-            
-            # Dropdown filter for URL
-            selected_url = st.sidebar.selectbox(
-                "Filter by URL:", 
-                options=data['URL'].unique(), 
-                index=0
-            )
-            data = data[data['URL'] == selected_url]
-            
-            # Slider in the sidebar for filtering by number of clicks
-            min_clicks = int(data['Clicks'].min())
-            max_clicks = int(data['Clicks'].max())
-            if min_clicks == max_clicks:
-                st.sidebar.text(f"Clicks: {min_clicks}")
-                clicks_range = (min_clicks, max_clicks)
-            else:
+            validation_message = validate_data(data)
+            if validation_message == "Data is valid!":
+                st.success(validation_message)
+                
+                # URL dropdown filter in the sidebar
+                selected_url = st.sidebar.selectbox(
+                    'Select a URL:',
+                    options=['All URLs'] + data['URL'].tolist(),
+                    format_func=lambda x: x if x != 'All URLs' else 'All URLs'
+                )
+
+                # If a specific URL is selected, filter the data by the selected URL
+                if selected_url != 'All URLs':
+                    data = data[data['URL'] == selected_url]
+                
+                # Slider in the sidebar for filtering by number of clicks
+                min_clicks = int(data['Clicks'].min())
+                max_clicks = int(data['Clicks'].max())
                 clicks_range = st.sidebar.slider(
                     "Filter by number of clicks:",
                     min_clicks,
                     max_clicks,
                     (min_clicks, max_clicks)
                 )
-            
-            # Filter data by selected range of clicks
-            data = data[(data['Clicks'] >= clicks_range[0]) & (data['Clicks'] <= clicks_range[1])]
-            
-            # Calculate average clicks and show URLs with below-average clicks
-            avg_clicks = data['Clicks'].mean()
-            st.sidebar.text(f"Average Clicks: {avg_clicks:.2f}")
-            below_avg_data = data[data['Clicks'] < avg_clicks]
-            st.sidebar.text("URLs with below average clicks:")
-            st.sidebar.write(below_avg_data.sort_values(by="Clicks")[['URL', 'Clicks']])
-            
-            # Display the filtered data
-            st.write(data[['URL', 'Clicks']])
-            
+
+                # Calculate the average clicks
+                avg_clicks = data['Clicks'].mean()
+                st.sidebar.text(f"Average Clicks: {avg_clicks:.2f}")
+
+                # Button to show URLs with lower than average clicks
+                if st.sidebar.button("Show URLs with less than average clicks"):
+                    lower_than_avg = data[data['Clicks'] < avg_clicks]
+                    sorted_data = lower_than_avg.sort_values(by='Clicks', ascending=True)
+
+                    for index, row in sorted_data.iterrows():
+                        st.write(row['URL'], "-", row['Clicks'], "clicks")
+                else:
+                    filtered_data = data[(data['Clicks'] >= clicks_range[0]) & (data['Clicks'] <= clicks_range[1])]
+
+                    for index, row in filtered_data.iterrows():
+                        st.write(row['URL'], "-", row['Clicks'], "clicks")
+
         except Exception as e:
-            st.write("There was an error loading the data. Please ensure the file is in the correct format.")
-            st.write("Details:", e)
+            st.error(f"An error occurred: {e}")
 
 if __name__ == "__main__":
     main()
-
