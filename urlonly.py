643a86a35c7f5147573785830e6a669384daba2f
seokeith <141214829+seokeith@users.noreply.github.com>
2023-08-08 07:43:19 +0100

    Update urlonly.py

--- urlonly.py
+++ urlonly.py
@@ -2,44 +2,35 @@ import streamlit as st
 import pandas as pd
 
 def calculate_click_comparison(df):
-    # Group by average rank and calculate mean clicks
-    avg_rank_clicks = df.groupby('Average Rank')['Clicks'].mean().reset_index()
-
-    # Group by impressions and calculate mean clicks
+    avg_position_clicks = df.groupby('Average Position')['Clicks'].mean().reset_index()
     impressions_clicks = df.groupby('Impressions')['Clicks'].mean().reset_index()
-
-    return avg_rank_clicks, impressions_clicks
+    return avg_position_clicks, impressions_clicks
 
 def main():
     st.title('URL Metrics Analysis')
-
-    # File Upload
-    st.write('Upload a CSV file with the following columns:')
-    st.write('- URLs')
-    st.write('- Clicks')
-    st.write('- Impressions')
-    st.write('- Average Rank')
-    st.write('- Click Through Rate')
     
-    uploaded_file = st.file_uploader('Choose a CSV file', type=['csv'])
+    st.sidebar.write('Upload a CSV file with the following columns:')
+    st.sidebar.write('- URL')
+    st.sidebar.write('- Clicks')
+    st.sidebar.write('- Impressions')
+    st.sidebar.write('- CTR')
+    st.sidebar.write('- Average Position')
+
+    uploaded_file = st.sidebar.file_uploader('Choose a CSV file', type=['csv'])
 
     if uploaded_file is not None:
         df = pd.read_csv(uploaded_file)
-
-        # Show the uploaded data
+        
         st.subheader('Uploaded Data:')
-        st.dataframe(df)
+        st.write(df)
 
-        # Calculate click comparisons
-        avg_rank_clicks, impressions_clicks = calculate_click_comparison(df)
+        avg_position_clicks, impressions_clicks = calculate_click_comparison(df)
 
-        # Show click comparisons based on average rank
-        st.subheader('Clicks Comparison based on Average Rank:')
-        st.dataframe(avg_rank_clicks)
+        st.subheader('Clicks Comparison based on Average Position:')
+        st.write(avg_position_clicks)
 
-        # Show click comparisons based on impressions
         st.subheader('Clicks Comparison based on Impressions:')
-        st.dataframe(impressions_clicks)
+        st.write(impressions_clicks)
 
 if __name__ == '__main__':
     main()
