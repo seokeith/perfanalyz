3bb9219c5a263b4b4390637db2c6436dd6530313
seokeith <141214829+seokeith@users.noreply.github.com>
2023-08-08 08:28:09 +0100

    Update urlonly.py

--- urlonly.py
+++ urlonly.py
@@ -1,6 +1,29 @@
 import streamlit as st
 import pandas as pd
 
+def validate_data(data):
+    # Check URL column for string format
+    if not all(isinstance(item, str) for item in data['URL']):
+        return 'URL column should only contain strings representing the web URL.'
+
+    # Check Clicks column for integer format
+    if not all(isinstance(item, (int, float)) and item.is_integer() for item in data['Clicks']):
+        return 'Clicks column should only contain integers representing the number of clicks.'
+
+    # Check Impressions column for integer format
+    if not all(isinstance(item, (int, float)) and item.is_integer() for item in data['Impressions']):
+        return 'Impressions column should only contain integers representing the number of impressions.'
+
+    # Check CTR column for float format between 0.0 and 1.0
+    if not all(isinstance(item, float) and 0.0 <= item <= 1.0 for item in data['CTR']):
+        return 'CTR column should only contain floats between 0.0 and 1.0 representing the Click Through Rate.'
+
+    # Check Position column for float format
+    if not all(isinstance(item, float) for item in data['Position']):
+        return 'Position column should only contain floats representing the rank or position of the URL.'
+
+    return None  # If all validations pass
+
 def main():
     st.title('CSV Data Viewer')
 
@@ -28,11 +51,15 @@ def main():
         if not all(col in data.columns for col in expected_columns):
             st.error('Invalid CSV file format. Please make sure the CSV has the required columns.')
             return
-        
-        # Display the URLs
+
+        # Perform additional validation on data columns
+        validation_message = validate_data(data)
+        if validation_message:
+            st.error(validation_message)
+            return
+
+        # Display the URLs and entire data
         st.write(data['URL'])
-        
-        # Display the entire data
         st.write(data)
 
 if __name__ == '__main__':
